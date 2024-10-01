1b931e011792192ce54b18d4f334912a4c3c6ad4
spappa3k <pappalardo.dev@gmail.com>
2024-10-01 13:25:25 +0200

    .

--- index.js
+++
@@ -1,6 +0,0 @@
-console.log("ciao");
-// 1. creare cartella
-// 2. npm int per creare il package.josn
-// 3. npm i express
-// 4. npm i ts-node per l'utilizzo di typescript
-// 5. npm i nodemon per autocompilare e eseguire(hotreload)
