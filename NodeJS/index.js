3a4058df2d7aab3bf2b5859c3232105554dbc015
spappa3k <pappalardo.dev@gmail.com>
2024-10-01 15:26:51 +0200

    .

---
+++ NodeJS/index.js
@@ -0,0 +1,8 @@
+
+// 1. creare la cartella del progetto
+// 2. npm init per creare il package.json
+// 3. npm i express
+// 4. npm i ts-node (per l'utilizzo di typescript)
+// 5. npm i nodemon (per l'hotreload)
+// 6. eseguo l'app npx nodemon file.ts
+// 7. npm i @types/express (contiene la definizione dei tipi di express in Typescript)
